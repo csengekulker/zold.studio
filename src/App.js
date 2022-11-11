fc89f5c1536680788a72460d6562d4e8a2364b53
csengekulker <k71608883429@ktch.hu>
2022-11-11 14:18:46 +0100

    react router dom v5 usage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router , Route, Switch, Link } from 'react-router-dom'
 import { Home, About, Gallery, Booking } from "./pages"
 
 function App() {
   return (
-    <Home />
+    <>
+      <h1>TODO: Header component</h1>
+      
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/us">About</a></li>
+          <li><a href="/booking">Booking</a></li>
+          <li><a href="/gallery">Gallery</a></li>
+        </ul>
+      </nav>
+
+      <Router>
+        <Switch>
+          <Route path='/'><Home /></Route>
+          <Route path='/us'><About /></Route>
+          <Route path='/booking'><Booking /></Route>
+          <Route path='/gallery'><Gallery /></Route>
+        </Switch>
+      </Router>
+
+    </>
     
+
   );
 }
 
