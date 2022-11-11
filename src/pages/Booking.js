6e7d831cd80cdc762176c815f7dc63083051e306
csengekulker <k71608883429@ktch.hu>
2022-11-11 14:08:48 +0100

    basic pages for routing

---
+++ src/pages/Booking.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Booking () {
+    return <h2>Booking page</h2>
+}
+
+export default Booking
---
+++ src/pages/Gallery.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Gallery () {
+    return <h2>Gallery page</h2>
+}
+
+export default Gallery
