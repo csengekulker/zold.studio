65c0c52370e2ae7809e8823ec44e36633b051d7e
csengekulker <k71608883429@ktch.hu>
2022-11-18 11:04:47 +0100

    FIXME: mui styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { AppBar, Box, Typography } from '@mui/material'
- 
+// import { makeStyles, useTheme } from '@material-ui/styles'
+import useStyles from './useStyles'
+
+
 function Header (props) {
+
+    const classes = useStyles()
 
     return (
         <Box style={{ backgroundColor: 'blue'}}>
-            <AppBar position='static'>
+            <AppBar position='static' className={classes.className}>
                 <Typography>zold pont studio</Typography>
             </AppBar>
         </Box>
---
+++ src/components/useStyles.js
@@ -0,0 +1,10 @@
+import { makeStyles, useTheme } from '@mui/material/styles'
+
+const useStyles = makeStyles(theme => ({
+    className: {
+        width: '500px',
+        backgroundColor: 'blue'
+    }
+}))
+
+export default useStyles
